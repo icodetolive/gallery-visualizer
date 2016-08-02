ba4b11fd41189530d527421a45de07f90b28d8fe
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-08-01 22:05:33 -0700

    Init Commit

---
+++ Gallery Visualizer/FlickrWebServiceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FlickrWebServiceViewController.swift
+//  Gallery Visualizer
+//
+//  Created by Sugandha Naolekar on 7/31/16.
+//  Copyright © 2016 icode. All rights reserved.
+//
+
+import UIKit
+
+class FlickrWebServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# Gallery Visualizer
+
+This repository implements Flickr Web Service/API to display images from a certain gallery. 
+
+## Summary
+
+It demonstrates various concepts of iOS networking like:
+
+* Building a URL comprising of various components like: protocol, host, path, method and query string parameters
+* Parsing the URL
+* Making a HTTP request
+* Fetching the result data in Json format
+* Interpreting it and picking the required fields
+
+## Development Environment
+IDE: Xcode 7.3 
+Language: Swift 2.2
+
+
+
