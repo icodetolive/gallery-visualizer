c0586cf590611e16744ecd5ca860f0af5b10a1b2
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-08-13 19:29:28 -0700

    FEAT: Add IBoutlets and IBActions

--- Gallery Visualizer/FlickrWebServiceViewController.swift
+++ Gallery Visualizer/FlickrWebServiceViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class FlickrWebServiceViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var grabImageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func grabNewImage() {
+        
     }
-
 
 }
 
